72d3c68c318741406107b0dea52c61c237486b4e
Кирилл Чуркин <chu13kir@yandex.ru>
2023-09-01 02:42:59 +0300

    update concurrent lib

---
+++ concurrent/bool.go
@@ -0,0 +1,26 @@
+package concurrent
+
+import "sync"
+
+type AtomicBool struct {
+	value bool
+	mutex *sync.RWMutex
+}
+
+func NewAtomicBool() *AtomicBool {
+	return &AtomicBool{
+		mutex: &sync.RWMutex{},
+	}
+}
+
+func (atomic *AtomicBool) Get() bool {
+	atomic.mutex.RLock()
+	defer atomic.mutex.RUnlock()
+	return atomic.value
+}
+
+func (atomic *AtomicBool) Set(value bool) {
+	atomic.mutex.Lock()
+	defer atomic.mutex.Unlock()
+	atomic.value = value
+}
