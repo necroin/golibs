c170410348492d1b1fdcd69d02774e4f9bbf2e02
Kirill Churkin <kirill.churkin@corp.mail.ru>
2024-02-19 13:29:05 +0300

    add containers lib

---
+++ containers/map.go
@@ -0,0 +1,9 @@
+package containers
+
+type Map[K comparable, V any] struct {
+	values map[K]V
+}
+
+func NewMap[K comparable, V any]() *Map[] {
+
+}
---
+++ containers/multimap.go
@@ -0,0 +1,8 @@
+package containers
+
+type MultiKeyMap[V any] struct {
+}
+
+func NewMultiKeyMap[V any]() *MultiKeyMap[V] {
+	return  
+}
