26f728be46a1af5e40c167afedc1ec2eb5fe1568
Кирилл Чуркин <chu13kir@yandex.ru>
2024-04-20 22:09:26 +0300

    add tests

--- findex/tests/findex_test.go
+++ findex/tests/findex_test.go
@@ -1,7 +1,7 @@
 package tests
 
 import (
-	"fmt"
+	"slices"
 	"testing"
 
 	"github.com/necroin/golibs/findex"
@@ -18,5 +18,24 @@ func TestMain(t *testing.T) {
 		t.Fatal(err)
 	}
 	indexedFile.Index()
-	fmt.Println(indexedFile.Find("110"))
+
+	compareDataMap := map[string][]string{
+		"100": {"100", "Marylou", "Judye", "Marylou.Judye@yopmail.com", "Marylou.Judye@gmail.com", "police officer"},
+		"101": {"101", "Robbi", "Scammon", "Robbi.Scammon@yopmail.com", "Robbi.Scammon@gmail.com", "firefighter"},
+		"123": {"123", "Steffane", "Atonsah", "Steffane.Atonsah@yopmail.com", "Steffane.Atonsah@gmail.com", "police officer"},
+		"225": {"225", "Viviene", "Roche", "Viviene.Roche@yopmail.com", "Viviene.Roche@gmail.com", "developer"},
+		"254": {"254", "Pollyanna", "Chapland", "Pollyanna.Chapland@yopmail.com", "Pollyanna.Chapland@gmail.com", "police officer"},
+		"291": {"291", "Sissy", "Eachern", "Sissy.Eachern@yopmail.com", "Sissy.Eachern@gmail.com", "police officer"},
+		"350": {"350", "Sashenka", "Ferino", "Sashenka.Ferino@yopmail.com", "Sashenka.Ferino@gmail.com", "worker"},
+	}
+
+	for index, data := range compareDataMap {
+		indexedData, err := indexedFile.Find(index)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if !slices.Equal(indexedData, data) {
+			t.Fatalf("%s != %s", indexedData, data)
+		}
+	}
 }
