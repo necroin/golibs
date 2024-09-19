47d8faa9fc6e5028d7805d86e6e275e8d2207cf6
Kirill Churkin <chu13kir@yandex.ru>
2024-09-19 11:28:59 +0300

    update sql schema

--- sqlschema/utils.go
+++
@@ -1,9 +0,0 @@
-package sqlschema
-
-func MapSchema(schema []Table) map[string]Table {
-	result := map[string]Table{}
-	for _, table := range schema {
-		result[table.Name] = table
-	}
-	return result
-}
