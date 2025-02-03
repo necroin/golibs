fdf551bb25f7c8e9b5c6ef82544a02cf56ed01e5
Kirill Churkin <kirill.churkin@corp.mail.ru>
2025-02-03 11:33:13 +0300

    update converter

--- tests/msg_convert/protobuf/converter_test.go
+++ tests/msg_convert/protobuf/converter_test.go
@@ -1,7 +1,8 @@
 package msg_convert_protobuf_tests
 
 import (
-	"os"
+	"bytes"
+	"strings"
 	"testing"
 
 	msg_convert_protobuf "github.com/necroin/golibs/libs/msg_convert/protobuf"
@@ -10,8 +11,44 @@ import (
 func TestJson(t *testing.T) {
 	message := "field_1:\"field_1_value\" field_2:\"12345678\" field_3:12345678 field_4:<field_4_field_1:\"field_4_field_1_value\" field_4_field_2:\"12345678\" field_4_field_3:12345678 field_4_field_4:true> field_4:<field_4_field_1:\"field_4_field_1_value_2\" field_4_field_2:\"87654321\" field_4_field_3:87654321 field_4_field_4:false>"
 
+	expected := `
+		{
+    	    "field_1": "field_1_value",
+    	    "field_2": "12345678",
+    	    "field_3": "12345678",
+    	    "field_4": [
+    	            {
+    	                    "field_4_field_1": "field_4_field_1_value",
+    	                    "field_4_field_2": "12345678",
+    	                    "field_4_field_3": "12345678",
+    	                    "field_4_field_4": "true"
+    	            },
+    	            {
+    	                    "field_4_field_1": "field_4_field_1_value_2",
+    	                    "field_4_field_2": "87654321",
+    	                    "field_4_field_3": "87654321",
+    	                    "field_4_field_4": "false"
+    	            }
+    	    ]
+		}
+	`
+
+	expected = strings.ReplaceAll(expected, "\t", "")
+	expected = strings.ReplaceAll(expected, "\n", "")
+	expected = strings.ReplaceAll(expected, " ", "")
+
 	converter := msg_convert_protobuf.NewConverter()
-	if err := converter.ToJson([]byte(message), os.Stdout); err != nil {
+	resultBuffer := &bytes.Buffer{}
+	if err := converter.ToJson([]byte(message), resultBuffer); err != nil {
 		t.Fatal(err)
+	}
+
+	result := resultBuffer.String()
+	result = strings.ReplaceAll(result, "\t", "")
+	result = strings.ReplaceAll(result, "\n", "")
+	result = strings.ReplaceAll(result, " ", "")
+
+	if result != expected {
+		t.Fatalf("messages not equal, %s != %s", result, expected)
 	}
 }
